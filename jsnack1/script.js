6ff4d3c7bcb3dbf7f18d25e0fcd3db44cbc19891
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-11 13:04:10 +0100

    add jsnack1 complete

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,15 @@ console.log("JS OK!");
 
 // consegna:
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+
+let somma = 0;
+const numeri = [4, 7, 3, 5, 6, 8, 5, 6, 2, 4, 4, 5, 7, 9];
+console.log(numeri);
+
+for (let i = 0; i < numeri.length; i++) {
+  if (i % 2 !== 0) {
+    somma += numeri[i];
+  }
+}
+
+console.log(`somma numeri in posizione dispari : ${somma}`);
