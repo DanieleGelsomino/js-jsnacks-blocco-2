d6d8af9d081b573ee564723f8729bd13f9b6c872
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-11 15:16:15 +0100

    add jsnack3 complete

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,3 +5,11 @@ console.log("JS OK!");
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
+
+const listOne = ["ciao", 14, "Zlatan", 11, "Milan"];
+const ListTwo = [9, "Giroud"];
+
+while (ListTwo.length < listOne.length) {
+  ListTwo.push(Math.floor(Math.random() * 100) + 1);
+  console.log(ListTwo);
+}
