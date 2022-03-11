6c2c0f14dff66e696cbf3342ed5333321fc14756
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-11 13:24:45 +0100

    add versione for  jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,3 +5,17 @@ console.log("JS OK!");
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+// VERSIONE FOR
+
+let somma = 0;
+
+for (let inputNumero = 0; inputNumero < 5; inputNumero++) {
+  const inputNumero = parseInt(prompt("digita un numero"));
+  if (isNaN(inputNumero)) {
+    alert("inserisci un numero!");
+  } else {
+    somma += inputNumero;
+    console.log(`la somma dei numeri inseriti è : ${somma}`);
+  }
+}
